9f60b7557a9ea412ea16329b7af6aa2c651fa1f2
Adnan <34342323+AdnanKasujja@users.noreply.github.com>
2020-03-23 21:56:02 -0500

    Add files via upload

---
+++ Assets/NetworkCharacter.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkCharacter : MonoBehaviour
+{
+	public NetworkMan netMan;
+	public string network_id { get; private set; }
+	public bool controllable { get; private set; }
+	private Vector3 position;
+	private Vector3 euler;
+	private Quaternion rotation;
+
+	private void Start()
+	{
+		if(controllable)
+			InvokeRepeating("NetUpdateTransform", 1,0.03f);
+		position = transform.position;
+		rotation = transform.rotation;
+	}
+	private void Update()
+	{
+		Control();
+	}
+	private void Control()
+	{
+		if (!controllable)
+			return;
+
+		if (Input.GetKey(KeyCode.W))
+		{
+			position += transform.TransformDirection(Vector3.forward) * Time.deltaTime;
+			//transform.Translate(Vector3.forward * Time.deltaTime);     //Moves along local y at speed per second
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			position += transform.TransformDirection(Vector3.back) * Time.deltaTime;
+			//transform.Translate(-Vector3.forward * Time.deltaTime) //Moves along local y at speed per second;
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			euler += Vector3.up * Time.deltaTime * 90;
+			rotation = Quaternion.Euler(euler);
+			//transform.Rotate(Vector3.up * Time.deltaTime * 90); ??rotate;
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			euler += Vector3.down * Time.deltaTime * 90;
+			rotation = Quaternion.Euler(euler);
+			//transform.Rotate(-Vector3.up * Time.deltaTime * 90); //rotate;
+		}
+	}
+	public void SetNetMan(NetworkMan netman)
+	{
+		netMan = netman;
+	}
+	public void SetNetworkID(string id)
+	{
+		network_id = id;
+	}
+	public void SetControllable(bool control)
+	{
+		controllable = control;
+	}
+
+	public void NetUpdateTransform()
+	{
+		netMan.SendTransform(position, rotation, network_id);
+	}
+}
--- Assets/NetworkMan.cs
+++ Assets/NetworkMan.cs
@@ -9,12 +9,28 @@ using System.Net;
 public class NetworkMan : MonoBehaviour
 {
     public UdpClient udp;
+    public GameObject playerPrefab;
+    private string ownID;
+
+    private Dictionary<string, GameObject> connectedPlayers;
+    private Queue<NetworkMessage.Player> newPlayers;
+    private Queue<string> disconnectedPlayers;
+
+    public NetworkMessage.UpdatedPlayer latestUpdates;
+
     // Start is called before the first frame update
     void Start()
     {
+        connectedPlayers = new Dictionary<string, GameObject>();
+        connectedPlayers.Clear();
+        newPlayers = new Queue<NetworkMessage.Player>();
+        newPlayers.Clear();
+        disconnectedPlayers = new Queue<string>();
+        disconnectedPlayers.Clear();
+
         udp = new UdpClient();
         
-        udp.Connect("PUT_IP_ADDRESS_HERE",12345);
+        udp.Connect("3.209.132.25", 12345);
 
         Byte[] sendBytes = Encoding.ASCII.GetBytes("connect");
       
@@ -25,99 +41,166 @@ public class NetworkMan : MonoBehaviour
         InvokeRepeating("HeartBeat", 1, 1);
     }
 
-    void OnDestroy(){
+    void OnDestroy()
+    {
         udp.Dispose();
+        connectedPlayers.Clear();
+        disconnectedPlayers.Clear();
+        newPlayers.Clear();
     }
 
-
-    public enum commands{
-        NEW_CLIENT,
-        UPDATE
-    };
-    
-    [Serializable]
-    public class Message{
-        public commands cmd;
-    }
-    
-    [Serializable]
-    public class Player{
-        public string id;
-        public struct receivedColor{
-            public float R;
-            public float G;
-            public float B;
-        }
-        public receivedColor color;        
-    }
-
-    [Serializable]
-    public class NewPlayer{
-        
-    }
-
-    [Serializable]
-    public class GameState{
-        public Player[] players;
-    }
-
-    public Message latestMessage;
-    public GameState lastestGameState;
-    void OnReceived(IAsyncResult result){
-        // this is what had been passed into BeginReceive as the second parameter:
+    void OnReceived(IAsyncResult result)
+    {
+    //  passed into BeginReceive as a second parameter:
         UdpClient socket = result.AsyncState as UdpClient;
         
-        // points towards whoever had sent the message:
+    // points towards whom sent the message:
         IPEndPoint source = new IPEndPoint(0, 0);
 
-        // get the actual message and fill out the source:
+    // get message fill source:
         byte[] message = socket.EndReceive(result, ref source);
         
-        // do what you'd like with `message` here:
+    // Mess around with the message here
         string returnData = Encoding.ASCII.GetString(message);
         Debug.Log("Got this: " + returnData);
-        
-        latestMessage = JsonUtility.FromJson<Message>(returnData);
-        try{
-            switch(latestMessage.cmd){
-                case commands.NEW_CLIENT:
+
+        NetworkMessage.State latestState = JsonUtility.FromJson<NetworkMessage.State>(returnData);
+        try
+        {
+            switch(latestState.cmd)
+            {
+                case NetworkMessage.Commands.NEW_CLIENT:
+                    Debug.Log("New Client");
+                    NetworkMessage.NewPlayer newPlayer = JsonUtility.FromJson<NetworkMessage.NewPlayer>(returnData);
+                    newPlayers.Enqueue(newPlayer.player);
                     break;
-                case commands.UPDATE:
-                    lastestGameState = JsonUtility.FromJson<GameState>(returnData);
+                case NetworkMessage.Commands.UPDATE:
+                    Debug.Log("Update");
+                    latestUpdates = JsonUtility.FromJson<NetworkMessage.UpdatedPlayer>(returnData);
+                    break;
+                case NetworkMessage.Commands.CLIENT_DROPPED:
+                    Debug.Log("Client Dropped");
+                    NetworkMessage.DisconnectedPlayer dropped = JsonUtility.FromJson<NetworkMessage.DisconnectedPlayer>(returnData);
+                    foreach (var p in dropped.players)
+                        disconnectedPlayers.Enqueue(p.id);
+                    break;
+                case NetworkMessage.Commands.CLIENT_LIST:
+                    Debug.Log("Client List");
+
+                    NetworkMessage.ConnectedPlayer clientList = JsonUtility.FromJson<NetworkMessage.ConnectedPlayer>(returnData);
+                    if (clientList.players.Length > 0)
+                    {
+                        foreach (var p in clientList.players)
+                            newPlayers.Enqueue(p);
+                    }
+                    break;
+                case NetworkMessage.Commands.OWN_ID:
+                    Debug.Log("Connected");
+                    NetworkMessage.NewPlayer player = JsonUtility.FromJson<NetworkMessage.NewPlayer>(returnData);
+                    ownID = player.player.id;
+                    newPlayers.Enqueue(player.player);
                     break;
                 default:
                     Debug.Log("Error");
                     break;
             }
         }
-        catch (Exception e){
+        catch (Exception e)
+        {
             Debug.Log(e.ToString());
         }
         
-        // schedule the next receive operation once reading is done:
+    // Once reading is done, schedule next receive
         socket.BeginReceive(new AsyncCallback(OnReceived), socket);
     }
 
-    void SpawnPlayers(){
+    void SpawnPlayers(NetworkMessage.Player player)
+    {
+        if (connectedPlayers.ContainsKey(player.id))
+        {
+            Debug.Log("Already exists");
+            return;
+        }
 
+    //spawn game object
+        GameObject newPlayerGameObject = Instantiate(playerPrefab);
+
+    //Each player will get a different color
+        Color newColor = new Color(player.color.R, player.color.G, player.color.B);
+        newPlayerGameObject.GetComponent<Renderer>().material.SetColor("_Color", newColor);
+
+    //add the network id
+        NetworkCharacter character = newPlayerGameObject.GetComponent<NetworkCharacter>();
+        character.SetNetMan(this);
+        character.SetNetworkID(player.id);
+        character.SetControllable(player.id == ownID);
+        connectedPlayers.Add(player.id, newPlayerGameObject);
     }
 
-    void UpdatePlayers(){
+    void UpdatePlayers()
+    {
+        for (int i = 0; i < connectedPlayers.Count; i++)
+        {
+            if (latestUpdates.players.Length <= 0)
+                return;
 
+            if (i >= latestUpdates.players.Length)
+                return;
+            NetworkMessage.Player p = latestUpdates.players[i];
+            if (!connectedPlayers.ContainsKey(p.id))
+                continue;
+
+            GameObject player = connectedPlayers[p.id];
+            player.transform.position = p.position;
+            player.transform.rotation = p.rotation;
+        }
     }
 
-    void DestroyPlayers(){
-
+    void DestroyPlayers(string networkID)
+    {
+        Debug.Log("Destroyed player");
+        Destroy(connectedPlayers[networkID]);
+        connectedPlayers.Remove(networkID);
     }
     
-    void HeartBeat(){
+    void HeartBeat()
+    {
         Byte[] sendBytes = Encoding.ASCII.GetBytes("heartbeat");
         udp.Send(sendBytes, sendBytes.Length);
     }
+    public void SendTransform(Transform t, string id)
+    {
+        SendTransform(t.position, t.rotation, id);
+    }
+    public void SendTransform(Vector3 position, Quaternion rotation, string id)
+    {
+        NetworkMessage.PlayerData p = new NetworkMessage.PlayerData();
+        p.position = position;
+        p.rotation = rotation;
+        string jsonString = JsonUtility.ToJson(p);
+        Debug.Log(jsonString);
+        Byte[] sendBytes = Encoding.ASCII.GetBytes(jsonString);
+        udp.Send(sendBytes, sendBytes.Length);
+    }
 
-    void Update(){
-        SpawnPlayers();
+    private void Update()
+    {
+        if (newPlayers.Count > 0)
+        {
+            for (int i = 0; i < newPlayers.Count; i++)
+            {
+                NetworkMessage.Player newPlayer = newPlayers.Dequeue();
+                SpawnPlayers(newPlayer);
+            }
+        }
+        if (disconnectedPlayers.Count > 0)
+        {
+            for (int i = 0; i < disconnectedPlayers.Count; i++)
+            {
+                string diconnectedPlayerID = disconnectedPlayers.Dequeue();
+                DestroyPlayers(diconnectedPlayerID);
+            }
+        }
         UpdatePlayers();
-        DestroyPlayers();
     }
 }
